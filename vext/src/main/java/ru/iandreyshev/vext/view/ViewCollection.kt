345a3c7bb75dddb6c688583c757238585e257f9f
Ivan Andreyshev <ivan.andreyshev@ispringsolutions.com>
2018-10-09 14:17:05 +0300

    Refactor view collection methods

--- vext/src/main/java/ru/iandreyshev/vext/view/ViewCollection.kt
+++ vext/src/main/java/ru/iandreyshev/vext/view/ViewCollection.kt
@@ -5,17 +5,17 @@ import android.view.View
 fun visibleAll(vararg views: View) =
         views.forEach { it.visible() }
 
-fun visibleAll(views: List<View>) =
-        views.forEach { it.visible() }
+fun List<View>.visibleAll() =
+        forEach { it.visible() }
 
 fun goneAll(vararg views: View) =
         views.forEach { it.gone() }
 
-fun goneAll(views: List<View>) =
-        views.forEach { it.gone() }
+fun List<View>.goneAll() =
+        forEach { it.gone() }
 
 fun invisibleAll(vararg views: View) =
         views.forEach { it.invalidate() }
 
-fun invisibleAll(views: List<View>) =
-        views.forEach { it.invisible() }
+fun List<View>.invisibleAll() =
+        forEach { it.invisible() }
